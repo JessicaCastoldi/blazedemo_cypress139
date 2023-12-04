d25270e54274bce11f75e878275f624871bfb752
JessicaCastoldi <jecastold@hotmail.com>
2023-12-03 22:27:44 -0300

    Finalização dos testes

--- cypress/e2e/buscaVoos.cy.js
+++ cypress/e2e/buscaVoos.cy.js
@@ -1,5 +1,84 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+//Grupo de testes
+describe('Busca por voos', () => {
+
+  //Contexto/Cenário
+  context('Não Logado', () => {
+    //caminho para o arquivo com a massa de teste
+    const massaVoos = require('../fixtures/massaVoos')
+
+   // Inicialização dos testes //Antes dos Testes   
+  beforeEach(() => {
+    cy.visit('/') //abrir a página inicial da URL informanda no cypress.config.js
   })
-})
+
+  //Exemplo de teste simples
+  it('Buscar voos entre São Paolo e Cairo - Simples', () => {
+    //Verifica se o título da guia é igual 'BlazeDemo
+    cy.title().should('eq', 'BlazeDemo')
+
+    //Preenche a origem e o destino do voo
+    cy.get('select.form-inline')
+      .eq(0) //origem
+      .select('São Paolo')
+
+    cy.get('select.form-inline')
+      .eq(1) //destino
+      .select('Cairo')
+
+    //Aperta o botão Find Flights
+    cy.get('input.btn.btn-primary')
+      .click()
+
+    //Ocorre a transição para a página de Reserva
+    cy.title().should('eq', 'BlazeDemo - reserve')
+
+    //Verificar a frase de origem e destino do voo
+    cy.get('.container h3')
+      .should('have.text', 'Flights from São Paolo to Cairo: ' )
+
+    //Escolhe o primeiro voo
+    cy.get('tbody input[type="submit"]')
+      .eq(0)
+      .click()
+
+  });
+
+    //Exemplo de teste Data Driven
+
+      massaVoos.array.forEach(({origem, destino}) => {
+      
+    it(`Buscar voos entre ${origem} e ${destino} `, () => {
+      //Verifica se o título da guia é igual 'BlazeDemo
+      cy.title().should('eq', 'BlazeDemo')
+  
+      //Preenche a origem e o destino do voo
+      cy.get('select.form-inline')
+        .eq(0) //origem
+        .select(origem)
+               
+      cy.get('select.form-inline')
+        .eq(1) //destino
+        .select(destino)
+  
+      //Aperta o botão Find Flights
+      cy.get('input.btn.btn-primary')
+        .click()
+  
+      //Ocorre a transição para a página de Reserva
+      cy.title().should('eq', 'BlazeDemo - reserve')
+  
+      //Verificar a frase de origem e destino do voo
+      cy.get('.container h3')
+        .should('have.text', `Flights from ${origem} to ${destino}: ` )
+  
+      //Escolhe o primeiro voo
+      cy.get('tbody input[type="submit"]')
+        .eq(0)
+        .click()
+  
+      }); //fim do it
+    }) // fim do array (massa)
+
+  }) //fim do context
+  
+}) //fim do describe
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+
+//Em Erros desconhecidos, desligue o alerta
+Cypress.on('uncaught:exception', () => {
+    return false
+})
